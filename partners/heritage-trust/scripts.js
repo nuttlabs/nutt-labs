9f3e0bdcea62a90e6123af821de3f04c5a97fcaf
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2023-07-11 14:51:49 -0400

    Create scripts.js

---
+++ partners/heritage-trust/scripts.js
@@ -0,0 +1,12 @@
+
+letInterval( updateNextSectionLink, 10000 );
+
+function updateNextSectionLink() {
+  let nextSectionPropertyLink = document.querySelector('.parent-page__sections .notion-page__properties a');
+  
+  if( nextSectionPropertyLink ) {
+    console.log(nextSectionPropertyLink.href);
+  } else {
+    console.log("Nevermind.");
+  }
+}
