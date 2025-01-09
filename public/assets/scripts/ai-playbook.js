e21aede7e497a873053a51813c1c3a9fea4f9d03
nuttlabs <122705994+nuttlabs@users.noreply.github.com>
2025-01-08 20:54:16 -0500

    Prog

--- public/assets/scripts/ai-playbook.js
+++ public/assets/scripts/ai-playbook.js
@@ -1,5 +1,39 @@
 document.querySelectorAll('.pillar').forEach(pillar => {
+
+    let contentDiv = pillar.querySelector('.pillar__content');
+    let arrow = pillar.querySelector('.pillar__arrow');
+    
+    let scrollHeight = prepScrollHeight(contentDiv);
+
+
     pillar.addEventListener('click', () => {
-        pillar.toggleAttribute('expanded');
+        toggleContentDiv(contentDiv, scrollHeight, arrow);
     })
 })
+
+function prepScrollHeight(contentDiv) {
+
+    const scrollHeight = contentDiv.scrollHeight;
+    contentDiv.setAttribute('data-scrollHeight', scrollHeight);
+    contentDiv.style.height = '0px';
+    contentDiv.style.position = 'static';
+    contentDiv.style.opacity = '1';
+
+    return scrollHeight;
+
+}
+
+function toggleContentDiv(contentDiv, scrollHeight, arrow) {
+
+    let newHeight = 0;
+    let rotation = 0;
+
+     if(contentDiv.style.height == '0px') {
+        newHeight = scrollHeight;
+        rotation = 90;
+     }
+
+     contentDiv.style.height = newHeight + 'px';
+     arrow.style.rotate = rotation + 'deg';
+
+}
